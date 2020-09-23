be489e63ae4b49539937c4e62530b788dab0e4ff
Tomislav Mariceivc <tmarice@gmail.com>
2020-09-23 22:59:41 +0200

    Keys tests

--- pybitcoin/tests/keys/test_keys.py
+++ pybitcoin/tests/keys/test_keys.py
@@ -1,6 +1,14 @@
 from hypothesis import given, strategies as st, assume
+from pybitcoin.ecc import secp256k1, Point
 import pytest
-from pybitcoin.keys import base58check_encode, base58check_decode, BASE58_ALPHABET, Base58DecodeError
+from pybitcoin.keys import (
+    base58check_encode,
+    base58check_decode,
+    BASE58_ALPHABET,
+    Base58DecodeError,
+    PrivateKey,
+    InvalidKeyError,
+)
 
 
 @given(
@@ -20,3 +28,53 @@ def test_base58check_decode_bad_check(data):
     '''Check Base58DecodeError is raised if check digits are wrong.'''
     with pytest.raises(Base58DecodeError):
         base58check_decode(data)
+
+
+@given(k=st.integers(min_value=1, max_value=secp256k1.p - 1))
+def test_private_key_k_ok(k):
+    p = PrivateKey(k=k)
+
+    assert p.k == k
+
+
+def test_private_key_k_not_given():
+    p = PrivateKey()
+
+    assert 0 < p.k < secp256k1.p
+
+
+@given(
+    k_1=st.integers(max_value=0),
+    k_2=st.integers(min_value=secp256k1.p),
+)
+def test_private_key_k_invalid(k_1, k_2):
+    with pytest.raises(InvalidKeyError):
+        PrivateKey(k=k_1)
+
+    with pytest.raises(InvalidKeyError):
+        PrivateKey(k=k_2)
+
+
+@given(
+    k=st.integers(min_value=1, max_value=secp256k1.p - 1),
+    compressed=st.booleans(),
+)
+def test_generate_public_key(k, compressed):
+    prv = PrivateKey(k=k, compressed=compressed)
+    pub = prv.generate_public_key()
+
+    assert k * Point.gen() == Point(pub.x, pub.y)
+    assert prv.compressed == pub.compressed
+
+
+@given(
+    k=st.integers(min_value=1, max_value=secp256k1.p - 1),
+    compressed=st.booleans(),
+    testnet=st.booleans(),
+)
+def test_to_wif(k, compressed, testnet):
+    pass
+
+
+def test_from_wif():
+    pass
