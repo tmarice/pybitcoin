0b171cc6eed95b66b5879160f10095bb883ecd5a
Tomislav Mariceivc <tmarice@gmail.com>
2020-09-24 22:29:17 +0200

    ExtendedPrivateKey WIP

--- pybitcoin/keys.py
+++ pybitcoin/keys.py
@@ -138,21 +138,37 @@ class PrivateKey:
 
 
 class ExtendedPrivateKey(PrivateKey):
-    def __init__(self, chain_code: bytes, index: int, *args, **kwargs):
+    def __init__(self, chain_code: bytes, depth=0, index=0, parent_key=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.chain_code = chain_code
+        self.depth = depth
         self.index = index
+        self.parent_key = parent_key  # Parent public key
+
+        # TODO: refactor
+        if self.depth == 0:
+            assert self.index == 0
+            assert self.parent_key is None
 
     def generate_public_key(self):
         pub = super().generate_public_key()
 
         return ExtendedPublicKey(
-            x=pub._x, y=pub._y, compressed=pub._compressed, chain_code=self.chain_code, index=index
+            x=pub._x,
+            y=pub._y,
+            compressed=pub.compressed,
+            chain_code=self.chain_code,
         )
 
     def to_wif(self) -> str:
-        # TODO: Special packing for WIF
-        pass
+        version = b'\x04\x35\x83\x94' if self.testnet else b'\x04\x88\xAD\xE4'
+        depth = self.index.to_bytes(1, byteorder=BIG)
+        fingerprint = (0).to_bytes(4, byteorder=BIG) if self.depth == 0 else self.parent_key.identifier()[:4]
+        index = self.index.to_bytes(4, byterorder=BIG)
+        chain_code = self.chain_code.to_bytes(32, byteorder=BIG)
+        key_data = b'\x00' + self.k.to_bytes(32, byteorder=BIG)
+
+        return base58check_encode(version + depth + fingerprint + index + chain_code + key_data)
 
     @classmethod
     def from_wif(self, data: str):
